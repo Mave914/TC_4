0f1b54a4dc18c28faa3a9517d3f2ca6c5fbe6572
Maria Valeria <mavaleriavargas31@gmail.com>
2026-03-24 08:16:19 -0600

    .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using namespace std;
 int main()
 {
+    //ofstream salida("salida.txt")
+    //ifstream entrada ("entrada.txt")
+
+    /*
+     while(){
+     int = 1;
+     salida <<n++<<"."<<endl;
+     }
+
+     while(getline(entrada, linea)
+    salida <<n++<<"."<<endl;
+
+    entrada.close();
+    salida.close();
+     */
 
     return 0;
 }
